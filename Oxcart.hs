0d29cf33720070c5900bd5f1a2243a31103920dd
Chris Pressey <cpressey@catseye.tc>
2019-09-16 12:42:10 +0100

    Some development of idea into language, very crude so far.

--- Oxcart.hs
+++ Oxcart.hs
@@ -1,10 +1,18 @@
 module Oxcart where
 
-
 composeCPS f g = \x k -> (f x (\s -> (g s k)))
 
+nop  x k = k x
 incr x k = k (x+1)
 dbl  x k = k (x*2)
 
 t1 = incr 10 (\x -> dbl x id)
 t2 = (incr `composeCPS` dbl) 10 id
+
+m [] = nop
+m (x:xs) = (m' x) `composeCPS` (m xs)
+    where
+        m' '+' = incr
+        m' 'X' = dbl
+
+t3 = (m "+XX") 10 id
