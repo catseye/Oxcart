d646288c9acf39b8c9f6770e018ac59d4619cbd5
Chris Pressey <cpressey@catseye.tc>
2019-09-17 09:20:10 +0100

    Checkpoint.

--- Oxcart.hs
+++ Oxcart.hs
@@ -1,18 +1,27 @@
 module Oxcart where
 
-composeCPS f g = \x k -> (f x (\s -> (g s k)))
+data Elem = Num Integer
+          | Cont String
+   deriving (Show, Ord, Eq)
 
+type Op = [Elem] -> ([Elem] -> [Elem]) -> [Elem]
+
+
+nop :: Op
 nop  x k = k x
-incr x k = k (x+1)
-dbl  x k = k (x*2)
 
-t1 = incr 10 (\x -> dbl x id)
-t2 = (incr `composeCPS` dbl) 10 id
+incr :: Op
+incr ((Num x):xs) k = k (Num (x+1):xs)
+
+dbl :: Op
+dbl  ((Num x):xs) k = k (Num (x*2):xs)
 
 m [] = nop
 m (x:xs) = (m' x) `composeCPS` (m xs)
     where
         m' '+' = incr
         m' 'X' = dbl
+        composeCPS f g = \x k -> (f x (\s -> (g s k)))
 
-t3 = (m "+XX") 10 id
+
+test = m "+XX" [Num 0] id
