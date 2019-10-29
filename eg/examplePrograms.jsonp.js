68e07d935193f65a4da40479ed1a70e3459fce24
Chris Pressey <cpressey@catseye.tc>
2019-10-29 09:48:46 +0000

    Add while loop example, JSONP index of example programs.

---
+++ eg/examplePrograms.jsonp.js
@@ -0,0 +1,22 @@
+examplePrograms = [
+    {
+        "contents": "<0^^^^^^^^^^>\nS:<:v:)%\n",
+        "filename": "count-down.oxcart"
+    },
+    {
+        "contents": "S:0^%\n",
+        "filename": "infinite-loop.oxcart"
+    },
+    {
+        "contents": "0^^^^<0^^^^^^^^<0^^^^^^^^^^>>\n",
+        "filename": "manystacks.oxcart"
+    },
+    {
+        "contents": "0^^^^^\n",
+        "filename": "rudimentary.oxcart"
+    },
+    {
+        "contents": "0^^^^^\n(<0^00'$S:<:0v\\Y:v:0'%$\n",
+        "filename": "while-loop.oxcart"
+    }
+];
